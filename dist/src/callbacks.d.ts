aa38efd11e57a81e7296bd92fb0af5238d26a4f3
Jarrod Overson <jsoverson@gmail.com>
2021-04-21 11:43:45 -0400

    removed build directory from git

--- dist/src/callbacks.d.ts
+++
@@ -1,4 +0,0 @@
-import { WapcProtocol } from './protocol';
-import { WapcHost } from './wapc-host';
-export declare function generateWapcImports(instance: WapcHost): WapcProtocol & WebAssembly.ModuleImports;
-export declare function generateWASIImports(instance: WapcHost): WebAssembly.ModuleImports;
--- dist/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { instantiate, instantiateStreaming, WapcHost } from './wapc-host';
-export * as errors from './errors';
